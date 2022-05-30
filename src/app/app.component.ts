64ad18c5eaa812eb52e385c80e5062d50de8e9a3
Aghata Sophia <aghatasophia@gmail.com>
2022-05-30 18:19:54 -0300

    Add a simple translation button to change the language

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'search-bar-routes';
+  private currentLanguage: 'pt' | 'en' = 'en';
+
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang('en');
+    translate.use('en');
+  }
+
+  public onChangeLanguageButtonClick(): void {
+    const newLanguage = this.currentLanguage === 'en' ? 'pt' : 'en';
+    this.currentLanguage = newLanguage;
+    this.translate.use(newLanguage);
+  }
 }
